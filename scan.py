928a89b11ba569c9cc6e87a4fb677ac8ab4276a8
Robert Karl <robertkarl@users.noreply.github.com>
2017-12-22 18:22:37 -0800

    format the output nicely

--- scan.py
+++ scan.py
@@ -5,6 +5,8 @@ import eyed3
 import multiprocessing
 import argparse
 import itertools
+import logging
+logging.disable(logging.WARNING)
 
 def process_many(filenames):
     answer = set()
@@ -16,7 +18,9 @@ def process_one(fname, answer):
     try:
         tag = eyed3.load(fname).tag
         components = os.path.dirname(fname).split("/")
-        answer.add((tag.artist, tag.album, os.path.join(*components[:-1])))
+        artist = tag.artist if tag.artist is not None else "Unknown Artist"
+        album = tag.album if tag.album is not None else "Unknown Album"
+        answer.add((artist, album, fname))
     except AttributeError:
         pass
 
@@ -25,23 +29,22 @@ def partition(inputs, num_buckets):
     for index, value in enumerate(inputs):
         buckets[index % num_buckets].append(value)
     return buckets
-    
 
-def collect_fnames():
+def collect_fnames(path):
     paths = []
-    for root, dirs, files in os.walk('/Volumes/Music/'):
+    for root, dirs, files in os.walk(path):
         for file in files:
             path = os.path.join(root, file)
             if path[-3:] == "mp3":
                 paths.append(path)
     return paths
-    
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--num_threads", type=int, default=multiprocessing.cpu_count())
+    parser.add_argument("--path", type=str)
     args = parser.parse_args()
-    partitioned_stuff = [[i] for i in partition(collect_fnames(), args.num_threads)]
+    partitioned_stuff = [[i] for i in partition(collect_fnames(args.path), args.num_threads)]
     outputs = [[] for i in range(args.num_threads)]
     with multiprocessing.Pool(processes=args.num_threads) as pool:                                                                   
         result = pool.starmap(process_many, partitioned_stuff)
@@ -49,8 +52,19 @@ def main():
         for albumset in result[1:]:
             finaleresult = finalresult.union(albumset)
         sorted_albums = sorted(list(finalresult))
+        numcolumns = len(sorted_albums[0])
+        column_limits = [0 for i in range(numcolumns)]
+        for infotuple in sorted_albums:
+            for index, value in enumerate(infotuple):
+                if len(value) > column_limits[index]:
+                    column_limits[index] = len(value)
+        prefix = ""
+        for limit in column_limits[:-1]:
+            prefix += "{:<"
+            prefix += str(limit)
+            prefix += "}\t"
+        prefix += "{}"
         for artist, album, path in sorted_albums:
-            print("{}\t{}\t{}".format(artist, album, path))
- 
+            print(prefix.format(artist, album, path))
 if __name__ == "__main__":
     main()
